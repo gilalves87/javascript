538122cd6c8c2eefb3f6d6a3c1fc7f654568155c
Gilberto Martins <gamartins1987@gmail.com.br>
2020-08-22 20:21:55 -0300

    Finalizando joguinho

--- Jogo das Vogais/scripts.js
+++ Jogo das Vogais/scripts.js
@@ -7,12 +7,22 @@ let erros = document.querySelector("#errors");
 var contadorAcertos = 0;
 var contadorErros = 0;
 let resultado = document.querySelector("#resultado-final");
+let A = document.getElementById('A');
+let E = document.getElementById('E');
+let I = document.getElementById('I');
+let O = document.getElementById('O');
+let U = document.getElementById('U');
+let erro = document.getElementById('erro');
+let som = document.getElementById('som');
 
 function startGame() {
+    tocarSom();
     home.style.display = "none";
     game.style.display = "flex";
     result.style.display = "none";
     reset();
+    vogal.innerHTML = vogalRan(1);
+    alteraCor();
 }
 
 function reset() {
@@ -23,9 +33,12 @@ function reset() {
 }
 
 function backToHome() {
+    som.pause();
+    som.currentTime = 0;
     home.style.display = "flex";
     game.style.display = "none";
     result.style.display = "none";
+    
 }
 
 function goToResult() {
@@ -41,38 +54,62 @@ function mudarVogal(letra) {
         contadorAcertos = contadorAcertos + 1;
         acertos.innerHTML = contadorAcertos;
         // alert("Acertou " + contadorAcertos);
+        if (acertos.innerHTML === "10") {
+            verificarResultado();
+        } else {
+            vogal.innerHTML = vogalRan(1);
+            alteraCor();
+        }
+        
     } else {
         contadorErros = contadorErros + 1;
         erros.innerHTML = contadorErros;
         // alert("Errou " + contadorErros);
+        erro.play();
+        if (erros.innerHTML === "10") {
+            verificarResultado();
+        } 
     }
+    
+}
+
+function verificarResultado() {
     if (acertos.innerHTML === "10") {
         goToResult();
         reset();
         resultado.style.color = "green";
         resultado.innerHTML = "Parabéns! Você é muito inteligente! :)";
+        parabens.play();
     } else if (erros.innerHTML === "10") {
         goToResult();
         reset();
         resultado.style.color = "red";
         resultado.innerHTML = "Ops! Tem que estudar mais! :(";
+        estudarmais.play();
     }
-    vogal.innerHTML = vogalRan(1);
+}
+
+function alteraCor() {
     switch(vogal.innerHTML){
         case vogal.innerHTML = "A":
             vogal.style.color = "red";
+            A.play();
         break;
         case vogal.innerHTML = "E":
             vogal.style.color = "green";
+            E.play();
         break;
         case vogal.innerHTML = "I":
             vogal.style.color = "blue";
+            I.play();
         break;
         case vogal.innerHTML = "O":
             vogal.style.color = "yellow";
+            O.play();
         break;
         case vogal.innerHTML = "U":
             vogal.style.color = "brown";
+            U.play();
         break;
     }
 }
@@ -85,5 +122,9 @@ function vogalRan(length) {
        result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
     return result;
+ }
+
+ function tocarSom() {
+    som.play();
  }
  
